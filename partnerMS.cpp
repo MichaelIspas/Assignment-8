7d153a4120d5d06f87bcfebe63f0b3e52a1c4be5
Michael Ispas <michael.ispas@gmail.com>
2022-11-10 13:59:59 -0800

    Update partnerMS.cpp
    Added comments

--- partnerMS.cpp
+++ partnerMS.cpp
@@ -24,15 +24,21 @@ int main () {
  
   
     while (true){
+        // loop every 3 seconds
         this_thread::sleep_for(chrono::seconds(3));
 
         cout << "Checking for '1' in currentagent.txt..." << endl;
 
+        // open file, write from file to option variable, then close
         curragent_file.open("currentagent.txt", ios::in);
         curragent_file >> option;
         curragent_file.close();
 
+        // if option holds integer '1', trigger random selection process
         if (option == 1){
+            
+            // read from pre-made text file, randomly select based on line number
+            // and update a string variable with the information
             ifstream agent_file("agentlist.txt");
             int total_lines = 0;
                 
@@ -43,11 +49,13 @@ int main () {
    
             int random_number=rand()%total_lines;
 
+            // displays message when '1' is updated in text file and display agent name
             cout << endl;
             cout << "1 has been entered, selecting random agent..." << endl;
             cout << lines[random_number] << " has been chosen." << endl;
             cout << endl;
-
+            
+            //open file, erase file, write agent to file, then close file
             curragent_file.open("currentagent.txt", ios::out | ios::trunc);
             curragent_file << lines[random_number];
             curragent_file.close();
