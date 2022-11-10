91d5a11a3db8999e831de7fd770fd72b7a396152
Michael Ispas <michael.ispas@gmail.com>
2022-11-10 13:53:06 -0800

    Update partnerMS.cpp
    Removed user input options and added loop to continuously check for '1' to trigger the random selection process.

--- partnerMS.cpp
+++ partnerMS.cpp
@@ -3,8 +3,12 @@
 #include <string>
 #include <stdlib.h>
 #include <vector>
+#include <thread>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
+using namespace this_thread;
 
 void showlist();
 
@@ -13,77 +17,40 @@ int main () {
     fstream agent_file;
     fstream curragent_file;
     ifstream file_("agentlist.txt");
-    string userAnswer;
     int option;
     string line_;
     string line;
     vector<string> lines;
-
-    showlist();  
+ 
   
-    do{
-        cout << "Input 1 to randomly select an agent, 0 to view the list of agents, ";
-        cout << "or 2 to exit." << endl;
-        cin >> option;
-        cout << endl;
-    
-        if(option == 1){     
-        ifstream agent_file("agentlist.txt");
-        int total_lines = 0;
-            while(getline(agent_file,line))
-            {
-            total_lines++; 
-            lines.push_back(line);  
-            }
-   
-        int random_number=rand()%total_lines;
+    while (true){
+        this_thread::sleep_for(chrono::seconds(3));
 
-    
-        cout << lines[random_number] << endl;
+        cout << "Checking for '1' in currentagent.txt..." << endl;
 
-        curragent_file.open("currentagent.txt", ios::out | ios::trunc);
-        curragent_file << lines[random_number];
+        curragent_file.open("currentagent.txt", ios::in);
+        curragent_file >> option;
         curragent_file.close();
 
-        cout << endl;
-        } 
-    
-        else if(option == 0) {
-        cout << endl;
-        showlist();
-        }
+        if (option == 1){
+            ifstream agent_file("agentlist.txt");
+            int total_lines = 0;
+                
+                while(getline(agent_file,line)){
+                    total_lines++; 
+                    lines.push_back(line);  
+                }
+   
+            int random_number=rand()%total_lines;
 
-        else if (option == 2){
-        }
+            cout << endl;
+            cout << "1 has been entered, selecting random agent..." << endl;
+            cout << lines[random_number] << " has been chosen." << endl;
+            cout << endl;
 
-        else {
-            cout << "Invalid input. Please try again." << endl << endl;
+            curragent_file.open("currentagent.txt", ios::out | ios::trunc);
+            curragent_file << lines[random_number];
+            curragent_file.close();
         }
-
     }
-    while(option != 2);  
-    
-    return 0;
-}
-
-void showlist(){
-    ifstream file_("agentlist.txt");
-    fstream agent_file;
-    fstream curragent_file;
-    string line_;
-    string line;
-    vector<string> lines;
-
-    if(file_.is_open()){
-        while(getline(file_,line_)){
-            std::cout << line_<< '\n';
-        }
-        file_.close();
-    }
-        else{
-            std::cout<<"file is not open"<< '\n';
-            std::cin.get();
-        }
-
-    cout << endl;
 }
